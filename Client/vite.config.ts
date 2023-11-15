93dda0559c6475f27e81818d03620e18d66b9797
Maya <foenx0315@gmail.com>
2023-11-15 16:58:20 +0530

    stuff

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // You can add aliases for your project structure
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'esbuild', // You can customize the minification method
+  },
+});
